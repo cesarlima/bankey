0bfb0e84d14927b1b8358261f1266864a13b1cef
Cesar Lima <cesarcamposdelima@gmail.com>
2024-09-25 06:24:02 -0300

    Initial project setup

---
+++ Bankey/Bankey/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by MacPro on 25/09/24.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
