f62e0a84db683f2e3167fb60c22cfb49f14a2a89
Cesar Lima <cesarcamposdelima@gmail.com>
2024-09-26 06:39:14 -0300

    Add `LoginView` in `LoginViewController`

--- Bankey/Bankey/LoginViewController.swift
+++ Bankey/Bankey/LoginViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
+    private let loginView = LoginView(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension LoginViewController {
+    private func style() {
+
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
         
-        view.backgroundColor = .blue
+        NSLayoutConstraint.activate([
+            loginView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
     }
 }
